c738a425d20d7936841295b60e52e2181b13cff2
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-03 17:27:59 +0700

    Update Sider component

--- src/components/Share/Layout/Sider/index.js
+++ src/components/Share/Layout/Sider/index.js
@@ -1,48 +1,29 @@
 import React from 'react';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 
 import './style.scss';
 
-export default function Sider() {
-  function getItem(label, key, icon, children, type) {
-    return {
-      key,
-      icon,
-      children,
-      label,
-      type,
-    };
-  }
+export function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
 
-  const items = [
-    getItem('Navigation One', 'sub1', <MailOutlined />, [
-      getItem('Option 1', '1'),
-      getItem('Option 2', '2'),
-      getItem('Option 3', '3'),
-      getItem('Option 4', '4'),
-    ]),
-    getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-      getItem('Option 5', '5'),
-      getItem('Option 6', '6'),
-      getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
-    ]),
-    getItem('Navigation Three', 'sub4', <SettingOutlined />, [
-      getItem('Option 9', '9'),
-      getItem('Option 10', '10'),
-      getItem('Option 11', '11'),
-      getItem('Option 12', '12'),
-    ]),
-  ];
+export default function Sider({ items = [], onClick = () => {}, defaultOpenKeys = [], defaultSelectedKeys = []}) {
   return (
     <Menu
       style={{
         width: 256,
       }}
-      defaultOpenKeys={['sub1', 'sub2']}
-      selectedKeys={['1']}
+      defaultOpenKeys={defaultOpenKeys}
+      defaultSelectedKeys={defaultSelectedKeys}
       mode="inline"
       items={items}
+      onClick={onClick}
     />
   );
 }
