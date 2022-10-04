cd623957b25397bac0b60d3ad49d7b2e3ccca1b1
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-04 18:16:06 +0700

    Create ForgotAccountForm

---
+++ src/components/Share/ForgotAccountForm/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Button, Card, Form, Input } from 'antd';
+import { WEBSITE_NAME } from 'components/contants';
+
+export default function ForgotAccountForm({ onFinish = () => {}, redirectTo = () => {} }) {
+  return (
+    <Card>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Vui lòng nhập email của bạn!',
+            },
+          ]}
+        >
+          <Input placeholder={`Email đăng nhập ${WEBSITE_NAME}`}/>
+        </Form.Item>
+
+        <Form.Item
+          label="Tên tài khoản"
+          name="username"
+        >
+          <Input placeholder={`Tên đăng nhập ${WEBSITE_NAME}`}/>
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" size='large' htmlType="submit">
+            Lấy lại mật khẩu
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+}
