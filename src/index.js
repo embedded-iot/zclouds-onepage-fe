8941ef0c41baf8fa4436ba399e9a09590dabe5c5
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-04 18:27:14 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
@@ -20,15 +20,17 @@ import 'globalStyles.scss';
 const initialState = {};
 const store = configureStore(initialState, history);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const MOUNT_NODE = document.getElementById('root');
+
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ConnectedRouter history={history}>
         <App />
       </ConnectedRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  MOUNT_NODE
 );
 
 // If you want to start measuring performance in your app, pass a function
