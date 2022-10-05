80db3773f8624c1e7242d987389a5ec6d50ca311
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-05 17:58:29 +0700

    Update Header

--- src/components/Share/Layout/Header/index.js
+++ src/components/Share/Layout/Header/index.js
@@ -3,13 +3,14 @@ import UserInfo from 'components/Share/UserInfo';
 
 import './style.scss';
 
-export default function Header({ logoName, isLogin, currentUser, redirectTo, signOut }) {
+export default function Header({ logoName, isLogin, isAdmin, currentUser, redirectTo, signOut }) {
   return (
     <div className="header-wrapper">
       <div className='logo-portal'>
         <a href='/'>{ logoName }</a>
       </div>
       <UserInfo isLogin={isLogin}
+                isAdmin={isAdmin}
                 currentUser={currentUser}
                 redirectTo={redirectTo}
                 signOut={signOut}
