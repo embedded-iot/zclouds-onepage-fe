d691bbadfa9481973529de5554f72998640544c9
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-05 17:56:18 +0700

    Update contants.js

--- src/components/contants.js
+++ src/components/contants.js
@@ -1,2 +1,16 @@
 export const WEBSITE_NAME = 'LIKE68';
 export const WEBSITE_DOMAIN = 'like68.vn';
+export const ROUTERS = {
+  ROOT: '/',
+  LOGIN: '/login',
+  REGISTER: '/register',
+  FORGOT_ACCOUNT: '/forgot-account',
+  LOGOUT: '/logout',
+  ACCOUNT_INFO: '/account-info',
+  HISTORY_ORDERS: '/history-orders',
+  WALLET_WITH_TAB: '/wallet/:tab',
+  DEPOSIT_HISTORY: '/wallet/deposit-history',
+  DEPOSIT_METHODS: '/wallet/deposit-methods',
+  PRICES: '/prices',
+}
+
