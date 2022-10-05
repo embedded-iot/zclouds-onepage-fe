4f53843cc154b4ae278ab76b190ce5953c72a8ca
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-05 18:02:33 +0700

    Update UserInfo

--- src/components/Share/UserInfo/index.js
+++ src/components/Share/UserInfo/index.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import { Avatar, Button, Dropdown, Menu } from 'antd';
-import { LoginOutlined, DollarOutlined, UserAddOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons';
+import {
+  LoginOutlined,
+  DollarOutlined,
+  UserAddOutlined,
+  LogoutOutlined,
+  UserOutlined,
+  OrderedListOutlined, SwapOutlined,
+} from '@ant-design/icons';
+import { ROUTERS } from 'components/contants';
 
 import './style.scss';
 
-
-export default function UserInfo({ isLogin = false, currentUser = {}, redirectTo = () => {}, signOut = () => {}}) {
+export default function UserInfo({ isLogin = false, isAdmin = false, currentUser = {}, redirectTo = () => {}, signOut = () => {}}) {
   const handleMenuClick = (e) => {
     switch (e.key) {
-      case 'logout':
+      case ROUTERS.LOGOUT:
         signOut();
         break;
       default:
-        redirectTo('/account-info');
+        redirectTo(e.key);
         break;
     }
   }
+
+  const userMenuItems = isLogin && !isAdmin ? [
+    {
+      label: 'Lịch sử đơn hàng',
+      key: ROUTERS.HISTORY_ORDERS,
+      icon: <OrderedListOutlined />,
+    },
+    {
+      label: 'Lịch sử đơn nạp',
+      key: ROUTERS.DEPOSIT_HISTORY,
+      icon: <SwapOutlined />,
+    }
+  ] : [];
 
   const menu = (
     <Menu
@@ -23,14 +43,15 @@ export default function UserInfo({ isLogin = false, currentUser = {}, redirectTo
       items={[
         {
           label: 'Thông tin tài khoản',
-          key: 'account-info',
+          key: ROUTERS.ACCOUNT_INFO,
           icon: <UserOutlined />,
         },
+        ...userMenuItems,
         {
           label: 'Đăng xuất',
-          key: 'logout',
+          key: ROUTERS.LOGOUT,
           icon: <LogoutOutlined />,
-        }
+        },
       ]}
     />
   );
@@ -39,14 +60,19 @@ export default function UserInfo({ isLogin = false, currentUser = {}, redirectTo
     <div className="user-info-wrapper">
       { !isLogin && (
         <>
-          <Button icon={<UserAddOutlined />} onClick={() => redirectTo('/register')}>Đăng ký</Button>
-          <Button type="primary" icon={<LoginOutlined />} onClick={() => redirectTo('/login')}>Đăng nhập</Button>
-          <Button type="primary" danger icon={<DollarOutlined />} onClick={() => redirectTo('/prices')}>Bảng giá</Button>
+          <Button icon={<UserAddOutlined />} onClick={() => redirectTo(ROUTERS.REGISTER)}>Đăng ký</Button>
+          <Button type="primary" icon={<LoginOutlined />} onClick={() => redirectTo(ROUTERS.LOGIN)}>Đăng nhập</Button>
+          <Button type="primary" danger icon={<DollarOutlined />} onClick={() => redirectTo(ROUTERS.PRICES)}>Bảng giá</Button>
         </>
       )}
       { isLogin && (
         <>
-          <span>Xin chào: <b>{currentUser.name || 'USER'}</b> </span>
+          <span>Xin chào: <b>{currentUser.name || 'USER'}</b></span>
+          <span>Số dư: </span>
+          <Button type="primary" size="small" danger>
+            {currentUser.balance || '0'}đ
+          </Button>
+          <Button type="primary" size="small" icon={<DollarOutlined />} onClick={() => redirectTo(ROUTERS.DEPOSIT_METHODS)}>Nạp tiền</Button>
           <Dropdown
             overlay={menu}
             placement="bottomRight"
@@ -57,6 +83,7 @@ export default function UserInfo({ isLogin = false, currentUser = {}, redirectTo
             <Avatar
               style={{
                 backgroundColor: '#87d068',
+                cursor: 'pointer'
               }}
               icon={<UserOutlined />}
             />
