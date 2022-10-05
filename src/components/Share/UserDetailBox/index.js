cfd295edb53189adbad0684c4e5ac774ccdb41d3
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-05 18:00:23 +0700

    Update UserDetailBox

--- src/components/Share/UserDetailBox/index.js
+++ src/components/Share/UserDetailBox/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Descriptions } from 'antd';
+import { Avatar, Card, Col, Descriptions, Row } from 'antd';
 import { Typography } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 const { Text } = Typography;
 
 
 export default function UserDetailBox({ userInfo = {}}) {
   return (
-    <Descriptions column={3} bordered>
-      <Descriptions.Item label="Tên tài khoản">{userInfo.username || "-"}</Descriptions.Item>
-      <Descriptions.Item label="Thành viên">{userInfo.level || "VIP"}</Descriptions.Item>
-      <Descriptions.Item label="Số dư">
-        <Text type="danger">{userInfo.balance || "0"} đ</Text>
-      </Descriptions.Item>
-      <Descriptions.Item label="Quyển">{!!userInfo.role ? userInfo.role.toUpperCase() : "USER"}</Descriptions.Item>
-    </Descriptions>
+    <Card>
+      <Row>
+        <Col span={4}>
+          <Avatar
+            style={{
+              backgroundColor: '#87d068',
+            }}
+            icon={<UserOutlined />}
+            size={80}
+          />
+        </Col>
+        <Col span={20}>
+          <Descriptions column={2}>
+            <Descriptions.Item label="Tên tài khoản">{userInfo.username || "-"}</Descriptions.Item>
+            <Descriptions.Item label="Thành viên">{userInfo.level || "VIP"}</Descriptions.Item>
+            <Descriptions.Item label="Số dư">
+              <Text type="danger">{userInfo.balance || "0"} đ</Text>
+            </Descriptions.Item>
+            <Descriptions.Item label="Quyền">{!!userInfo.role ? userInfo.role.toUpperCase() : "USER"}</Descriptions.Item>
+          </Descriptions>
+        </Col>
+      </Row>
+    </Card>
   );
 }
