c569261f321ca44f1a3c3693371de1633621c4a7
Nguyen Van Quan <nguyenquan5895@gmail.com>
2022-10-06 22:43:10 +0700

    Update footer

--- src/components/Share/Layout/Footer/index.js
+++ src/components/Share/Layout/Footer/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { WEBSITE_DOMAIN } from 'components/contants';
+
+import './style.scss';
+import { Divider } from 'antd';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="footer-wrapper">
+      <div>Hệ thống hoạt động 24/24. Nhân viên hỗ trợ 09-18h từ T2-T7</div>
+      <Divider/>
+      <div>Dev by {WEBSITE_DOMAIN} | Điều khoản & Quy định | Chính sách bảo mật</div>
+    </div>
   );
 }
