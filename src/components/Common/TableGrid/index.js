f590d00b4b3456d091fac6d7f5eca72311b92320
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-06 17:46:34 +0700

    Update TableGrid

--- src/components/Common/TableGrid/index.js
+++ src/components/Common/TableGrid/index.js
@@ -1,37 +1,112 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Pagination, Table } from 'antd';
 
-export default function TableGrid(props) {
+import './style.scss';
+import { events } from 'utils';
+
+export default function TableGrid({
+                                    tableConfig = {},
+                                    paginationConfig = {},
+                                    actionButtonList = [],
+                                    defaultParams = {},
+                                    defaultData = {},
+                                    onSelectedItemsChange = () => {},
+                                    isShowPagination = true,
+                                    isAllowSelection = false,
+                                    RELOAD_EVENT_KEY = ''
+                                  }) {
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [params, setParams] = useState({
+    pageSize: 20,
+    pageNum: 1,
+    ...defaultParams
+  });
+  const [data, setData] = useState({
+    items: [],
+    totalCount: 0,
+    ...defaultData
+  });
+
+  const getDataFunc = useCallback(() => {
+    if (!tableConfig.getDataFunc) return;
+    tableConfig.getDataFunc(params, (response) => {
+      setData(response);
+      tableConfig.successCallback(response);
+    }, error => {
+      tableConfig.failureCallback(error);
+    })
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    getDataFunc();
+    if (!!RELOAD_EVENT_KEY) {
+      events.subscribe(RELOAD_EVENT_KEY, (newParams = {}) => {
+        setParams({ ...params, ...newParams});
+        console.log(params);
+        getDataFunc();
+      })
+    }
+
+    // eslint-disable-next-line
+  }, []);
 
   const onSelectChange = (newSelectedRowKeys) => {
     setSelectedRowKeys(newSelectedRowKeys);
+    onSelectedItemsChange(newSelectedRowKeys);
   };
 
   const rowSelection = {
+    show: isAllowSelection,
     selectedRowKeys,
     onChange: onSelectChange,
   };
   const hasSelected = selectedRowKeys.length > 0;
 
+  const onPaginationChange = (page, pageSize) => {
+    setParams({
+      ...params,
+      pageSize,
+      pageNum: pageSize !== params.pageSize ? 1 : page,
+    });
+    getDataFunc();
+  };
+
   return (
-    <div>
-      <div style={{ marginBottom: 16, }}>
-        <span
-          style={{
-            marginLeft: 8,
-          }}
-        >
-          {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
-        </span>
+    <div className="table-view-wrapper">
+      <div className="table-header">
+        {
+          !!actionButtonList.length && (
+            <div className="additional-button-list">
+              {
+                actionButtonList.map(button => button)
+              }
+            </div>
+          )
+        }
       </div>
-      <Table rowSelection={rowSelection} columns={[]} dataSource={[]} pagination={false} />
-      <Pagination
-        total={85}
-        showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
-        defaultPageSize={20}
-        defaultCurrent={1}
+      <div className="selected-item-label">
+        { hasSelected && `Đã chọn ${selectedRowKeys.length} phần tử`}
+      </div>
+      <Table rowSelection={rowSelection}
+             columns={tableConfig.columns}
+             dataSource={data.items}
+             pagination={false}
       />
+      <br/>
+      {
+        isShowPagination && !!data.totalCount && (
+          <Pagination
+            {...paginationConfig}
+            total={data.totalCount}
+            showTotal={(total, range) => `${range[0]} - ${range[1]} trong ${total} phần tử.`}
+            defaultPageSize={params.pageSize}
+            defaultCurrent={params.pageNum}
+            showSizeChanger
+            onChange={onPaginationChange}
+          />
+        )
+      }
     </div>
   );
 }
