5b684274d428f47e42abc1d120589e3dfd47ce81
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-06 17:50:14 +0700

    Update DepositHistoryTable

--- src/components/User/DepositHistoryTable/index.js
+++ src/components/User/DepositHistoryTable/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import TableGrid from 'components/Common/TableGrid';
+import { OrderUserService } from 'services';
+import { Button } from 'antd';
+import { events } from 'utils';
 
 const columns = [
   {
@@ -28,19 +31,49 @@ for (let i = 0; i < 46; i++) {
 }
 
 export default function DepositHistoryTable(props) {
-
+  const RELOAD_EVENT_KEY = 'RELOAD_EVENT_KEY';
   const tableConfig = {
-    getDataService: () => {},
     columns,
-    successCallback: () => {
-      return
+    getDataFunc: (params, successCallback, failureCallback) => {
+      OrderUserService.getOrders(params, successCallback, failureCallback)
     },
-    failureCallback: () => {},
+    successCallback: (response) => {
+      console.log(response);
+    },
+    failureCallback: (error) => {
+      console.log(error);
+    },
   };
 
+  const paginationConfig = {};
+
+  const onSelectedItemsChange = (selectedKeys) => {
+
+  }
+
+  const defaultData = {
+    items: data,
+    totalCount: 46,
+  }
+
+  const reloadTable = () => {
+    events.publish(RELOAD_EVENT_KEY, { searchText: 'sadasd'});
+  }
+
+  const actionButtonList = [
+    <Button key={1} onClick={reloadTable}>Reload</Button>,
+    <Button key={2}>ádasdad</Button>
+  ]
+
   return (
-    <TableGrid config={tableConfig}
-               actionButtonList={[]}
+    <TableGrid tableConfig={tableConfig}
+               paginationConfig={paginationConfig}
+               actionButtonList={actionButtonList}
+               defaultParams={{}}
+               defaultData={defaultData}
+               isShowPagination={false}
+               onSelectedItemsChange={onSelectedItemsChange}
+               RELOAD_EVENT_KEY={RELOAD_EVENT_KEY}
     />
   );
 }
