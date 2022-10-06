a67c0be8fbf09b26ccbb3ead4f3f9ba53f193a1e
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-06 17:54:20 +0700

    Update utils index

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,10 +2,12 @@ import history from './history';
 import loadable from './loadable';
 import { makeGetWithConfigs, makePostWithConfigs, makePutWithConfigs, makeDeleteWithConfigs } from './requests';
 import authentication from './authentication';
+import events from './events';
 
 export {
   history,
   authentication,
+  events,
   loadable,
   makeGetWithConfigs,
   makePostWithConfigs,
