395b584090e7005be9ecb900deeed53ab57384f0
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-11 17:54:49 +0700

    Update CreateServiceOrder

--- src/components/User/CreateServiceOrder/index.js
+++ src/components/User/CreateServiceOrder/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Col, Row } from 'antd';
+import { Col, notification, Row } from 'antd';
 import CreateViewYoutube from './CreateViewYoutube';
+import { OrderUserService } from 'services';
 
-export default function CreateServiceOrder(props) {
+export default function CreateServiceOrder({ productType, serviceId, products }) {
+  const selectedProduct = products.find(product => product.type === productType) || {};
+  const selectedService = (selectedProduct.services || []).find(service => service.id.toString() === serviceId) || {}
+  const offersOptions = (selectedService.offers || []).map(offer => ({
+    value: offer.id,
+    label: `${offer.name} (${offer.credit} đ)`,
+    credit: offer.credit,
+  }));
+  const onFinish = (values) => {
+    OrderUserService.createOrder(values, response => {
+      notification.success({
+        message: "Bạn đã tạo đơn hàng thành công!",
+      });
+    }, error => {
+      notification.error({
+        message: error.status && error.status.message ? error.status.message : "Không thể tạo đơn hàng bây giờ. Vui lòng thử lại sau!",
+      });
+    })
+  }
+
   return (
     <Row>
       <Col span={12}>
-        <CreateViewYoutube />
+        <CreateViewYoutube onFinish={onFinish} offersOptions={offersOptions} />
       </Col>
     </Row>
   );
