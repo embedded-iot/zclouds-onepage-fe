2002143f5e0d441bf40ae8ee95f708d4f9867a1f
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-11 17:52:09 +0700

    Update CreateViewYoutube.js

--- src/components/User/CreateServiceOrder/CreateViewYoutube.js
+++ src/components/User/CreateServiceOrder/CreateViewYoutube.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Alert, Button, Card, Form, Input, Select } from 'antd';
+import React, { useState } from 'react';
+import { Alert, Button, Card, Form, Input, InputNumber, Select } from 'antd';
 
-export default function CreateViewYoutube({ onFinish = () => {}, redirectTo = () => {} }) {
+export default function CreateViewYoutube({ onFinish = () => {}, offersOptions = [], redirectTo = () => {} }) {
+  const [totalCredit, setTotalCredit] = useState(0)
   const message = (
     <span>
       Bạn sử dụng dịch vụ nhớ đọc Thông tin gói và mục Lưu ý gói bên phải <br/>
@@ -10,6 +11,11 @@ export default function CreateViewYoutube({ onFinish = () => {}, redirectTo = ()
         *Lưu ý: Số view đã tăng trong phần lịch sử có thể không cập nhật khi đơn đang chạy, bạn hãy xem số view thực tế ở Video
     </span>
   );
+  const handlerValuesChange = (values, allValues) => {
+    const selectedOffer = !!allValues.offerId ? offersOptions.find(option => option.value === allValues.offerId) : {}
+    setTotalCredit((allValues.quantity || 0) * (selectedOffer.credit || 0));
+  }
+
   return (
     <Card>
       <Alert message={message} type="info" style={{marginBottom: 20}}/>
@@ -22,32 +28,52 @@ export default function CreateViewYoutube({ onFinish = () => {}, redirectTo = ()
           span: 16,
         }}
         onFinish={onFinish}
+        onValuesChange={handlerValuesChange}
         autoComplete="off"
         layout="vertical"
       >
         <Form.Item
           label="Link Youtube"
-          name="link"
+          name="targetLink"
+          rules={[
+            {
+              required: true,
+              message: 'Link Video không được để trống!',
+            },
+            {
+              type: 'url',
+              message: 'Link Video không hợp lệ!',
+            },
+          ]}
         >
           <Input placeholder="Link Video Youtube" />
         </Form.Item>
 
         <Form.Item
           label="Chọn gói"
-          name="package"
+          name="offerId"
+          rules={[
+            {
+              required: true,
+              message: 'Chọn gói không được để trống!',
+            },
+          ]}
         >
-          <Select>
-            <Select.Option value="package1">Hack Views youtube Server 1 - 50đ</Select.Option>
-            <Select.Option value="package2">Short Views youtube Server 2 - 50đ</Select.Option>
-            <Select.Option value="package3">Tăng giờ xem Youtube 4000h - 150đ</Select.Option>
-          </Select>
+          <Select placeholder="Chọn gói phù hợp" options={offersOptions}></Select>
         </Form.Item>
         <Form.Item
           label="Số lượng"
-          name="count"
+          name="quantity"
+          rules={[
+            {
+              required: true,
+              message: 'Số lượng không được để trống!',
+            },
+          ]}
         >
-          <Input placeholder="Số lượng" />
+          <InputNumber min={0} placeholder="Số lượng" />
         </Form.Item>
+        <p>Thanh toán: {totalCredit} đ</p>
         <Form.Item>
           <Button type="primary" size='large' htmlType="submit">
            ORDER NGAY
