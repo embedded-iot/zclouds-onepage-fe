528030d3b0aa83673e9f7f9f4baaa4b3ac15f8a3
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-11 17:56:21 +0700

    Update OrdersHistoryFilters

--- src/components/User/OrdersHistoryTable/OrdersHistoryFilters/index.js
+++ src/components/User/OrdersHistoryTable/OrdersHistoryFilters/index.js
@@ -1,37 +1,52 @@
 import React, { useState } from 'react';
 import { Button, Col, Row } from 'antd';
 import AutoCompleteInput from 'components/Common/AutoCompleteInput';
-import { ORDER_STATUS } from 'components/contants';
-import InputText from 'components/Common/InputText';
+import { ORDER_STATUS_OPTIONS } from 'components/contants';
 
-export default function OrdersHistoryFilters({ products = [], onChange = () => {} }) {
+export default function OrdersHistoryFilters({ productType = '', serviceId= '', products = [], onChange = () => {} }) {
   const [filters, setFilters] = useState({});
+  const [filtersInputValues, setFiltersInputValues] = useState({});
 
+  let productsOptions = [{ label: 'ALL', value: 'all'}];
   let servicesOptions = [];
   products.forEach(product => {
+    productsOptions.push({
+      label: product.type.toUpperCase(),
+      value: product.type,
+    })
     servicesOptions = [
       ...servicesOptions,
-      ...(product.services.map(service => ({ value: service.name }))),
+      ...(product.services.map(service => ({ label: service.name, value: service.id, type: product.type }))),
     ]
   })
-  const statusOptions = ORDER_STATUS;
+
+  const filterServicesByProductType  = (productType) => {
+    return servicesOptions.filter(service => (productType && productType !== 'all') ? service.type === productType : true);
+  }
+
+  const statusOptions = ORDER_STATUS_OPTIONS;
 
   const onChangeInput = (value, name) => {
-    console.log(value);
+    setFiltersInputValues(prevFilters => ({ ...prevFilters, [name]: value }));
+  }
+
+  const onSelectInput = (value, name, inputValue) => {
     setFilters(prevFilters => ({ ...prevFilters, [name]: value }));
   }
 
   const handlerSearch = () => {
-    console.log(filters);
+    onChange(filters);
   }
 
   const handlerClear = () => {
     setFilters({});
+    setFiltersInputValues({});
+    onChange({});
   }
 
   return (
     <Row gutter={10}>
-      <Col span={4}>
+      {/*<Col span={4}>
         <InputText placeholder="Tìm theo mã đơn hàng"
                    value={filters.orderId}
                    name="orderId"
@@ -44,21 +59,40 @@ export default function OrdersHistoryFilters({ products = [], onChange = () => {
                    name="postId"
                    onChange={onChangeInput}
         />
-      </Col>
-      <Col span={4}>
-        <AutoCompleteInput options={servicesOptions}
-                           value={filters.serviceId}
-                           placeholder="Tìm theo dịch vụ"
-                           name="serviceId"
-                           onChange={onChangeInput}
-        />
-      </Col>
+      </Col>*/}
+      {
+        !productType && (
+          <Col span={4}>
+            <AutoCompleteInput options={productsOptions}
+                               placeholder="Tìm theo loại dịch vụ"
+                               name="productType"
+                               value={filtersInputValues.productType || ''}
+                               onChange={onChangeInput}
+                               onSelect={onSelectInput}
+            />
+          </Col>
+        )
+      }
+      {
+        !serviceId && (
+          <Col span={4}>
+            <AutoCompleteInput options={filterServicesByProductType(filters.productType)}
+                               value={filtersInputValues.serviceId}
+                               placeholder="Tìm theo thể loại"
+                               name="serviceId"
+                               onChange={onChangeInput}
+                               onSelect={onSelectInput}
+            />
+          </Col>
+        )
+      }
       <Col span={4}>
         <AutoCompleteInput options={statusOptions}
-                           value={filters.status}
+                           value={filtersInputValues.status}
                            placeholder="Tìm theo trạng thái"
                            name="status"
                            onChange={onChangeInput}
+                           onSelect={onSelectInput}
         />
       </Col>
       <Col span={4}>
