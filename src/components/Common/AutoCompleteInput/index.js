267f759258999e57e058104cfa5cc0028b86098e
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-11 17:53:25 +0700

    Update AutoCompleteInput

--- src/components/Common/AutoCompleteInput/index.js
+++ src/components/Common/AutoCompleteInput/index.js
@@ -3,20 +3,28 @@ import { AutoComplete } from 'antd';
 
 import './style.scss';
 
-export default function AutoCompleteInput({ name, value, options, onChange, placeholder, ...restProps }) {
+export default function AutoCompleteInput({ str, name, value, options, onChange, onSelect, placeholder, ...restProps }) {
   const filterOptions = (inputValue, option) => {
-    return option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1;
+    return option.label.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1;
   }
-  const handlerOnchange = (value) => onChange(value, name);
+
+  const handlerOnChange = (inputValue) => {
+    onChange(inputValue, name);
+  };
+  const handlerOnSelect = (value, option) => {
+    onChange(option.label, name);
+    onSelect(option.value, name);
+  };
 
   return (
     <AutoComplete
-      className="auto-complete-input"
+      className={`auto-complete-input`}
       {...restProps}
       value={value}
       options={options}
       placeholder={placeholder}
-      onChange={handlerOnchange}
+      onChange={handlerOnChange}
+      onSelect={handlerOnSelect}
       filterOption={filterOptions}
     />
   )
