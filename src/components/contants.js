b12dde7e4310418cc8d7c9bec7196503b467b849
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-10-11 17:51:17 +0700

    Update contants.js

--- src/components/contants.js
+++ src/components/contants.js
@@ -8,7 +8,7 @@ export const ROUTERS = {
   LOGOUT: '/logout',
   ACCOUNT_INFO: '/account-info',
   SERVICES: `/services`,
-  DETAIL_SERVICE: `/services/:serviceKey/:serviceName`,
+  DETAIL_SERVICE: `/services/:productType/:serviceId/:serviceName`,
   ACCOUNT_ASSETS: '/account-assets/:tab',
   ACCOUNT_ASSETS_INVOICES_HISTORY: '/account-assets/deposits-history',
   ACCOUNT_ASSETS_INVOICES_METHODS: '/account-assets/deposit-methods',
@@ -27,19 +27,29 @@ export const ROUTERS = {
   DETAIL_POSTS: '/posts/:postKey/:postName',
 }
 
-export const ORDER_STATUS = [
-  { value: 'Hoàn thành' },
-  { value: 'Hủy' },
-  { value: 'Yêu cầu Hủy' },
-  { value: 'Tạm dừng' },
-  { value: 'Chờ hoàn tiền' },
-  { value: 'Đã hoàn tiền' },
-  { value: 'Tăng một phần' },
-  { value: 'Lên lịch' },
-  { value: 'Đang kiểm tra' },
-  { value: 'Đang xử lý...' },
-  { value: 'Đợi chạy' },
-  { value: 'Đang chạy' },
+export const ORDER_STATUS = {
+  REQUESTED: 'REQUESTED',
+  IN_PROGRESS: 'IN_PROGRESS',
+  DONE: 'DONE',
+  REJECTED: 'REJECTED',
+  CANCELED: 'CANCELED',
+};
+
+export const ORDER_STATUS_LABEL = {
+  [ORDER_STATUS.REQUESTED]: 'Đã gửi yêu cầu',
+  [ORDER_STATUS.IN_PROGRESS]: 'Đang xử lý...',
+  [ORDER_STATUS.DONE]: 'Hoàn thành',
+  [ORDER_STATUS.REJECTED]: 'Đã từ chối',
+  [ORDER_STATUS.CANCELED]: 'Đã hủy',
+}
+
+
+export const ORDER_STATUS_OPTIONS = [
+  { label: ORDER_STATUS_LABEL[ORDER_STATUS.REQUESTED], value: ORDER_STATUS.REQUESTED },
+  { label: ORDER_STATUS_LABEL[ORDER_STATUS.IN_PROGRESS], value: ORDER_STATUS.IN_PROGRESS },
+  { label: ORDER_STATUS_LABEL[ORDER_STATUS.DONE], value: ORDER_STATUS.DONE },
+  { label: ORDER_STATUS_LABEL[ORDER_STATUS.REJECTED], value: ORDER_STATUS.REJECTED },
+  { label: ORDER_STATUS_LABEL[ORDER_STATUS.CANCELED], value: ORDER_STATUS.CANCELED },
 ];
 
 export const PRODUCT_TYPES = {
@@ -50,7 +60,15 @@ export const PRODUCT_TYPES = {
 
 export const ACTIVE_PRODUCT_TYPES = [
   PRODUCT_TYPES.YOUTUBE,
+  // PRODUCT_TYPES.TIKTOK,
+  // PRODUCT_TYPES.SHOPPE,
 ];
+
+export const INVOICE_LABELS = {
+  TOP_UP: 'Nạp tiền',
+  DEDUCT: 'Rút tiền',
+  REFUND : 'Hoàn tiền',
+};
 
 export const POST_KEYS = {
   BUFF_FB_LIKE: 'buff-fb-like',
