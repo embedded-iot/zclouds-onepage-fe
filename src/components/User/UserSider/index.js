987654115500839cae68079be818bfc66c730792
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-02 17:57:12 +0700

    Update index.js

--- src/components/User/UserSider/index.js
+++ src/components/User/UserSider/index.js
@@ -1,98 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { YoutubeOutlined, TableOutlined, LinkOutlined,
-  OrderedListOutlined, SwapOutlined, DollarOutlined,
-  BellOutlined, FacebookOutlined, UserOutlined,
-  ContactsOutlined } from '@ant-design/icons';
+import React from 'react';
 import Sider, { getItem } from 'components/Share/Layout/Sider';
-import { PostService, ProductService } from 'services';
-import { ROUTERS, WEBSITE_DOMAIN } from 'components/contants';
+import { ROUTERS } from 'components/contants';
 
 import "./style.scss";
 
 export default function UserSider({ selectedRouters = [], redirectTo = () => {}, setGlobalStore = () => {} }) {
-  const [products, setProducts] = useState(null);
-  const [posts, setPosts] = useState(null);
-  const getProducts = () => {
-    ProductService.getProducts(response => {
-      const productList = response.products.map(product => {
-        return {
-          ...product,
-          key: product.type,
-          services: product.services.map((service, serviceIndex) => {
-            return ({
-              ...service,
-              key: `${ROUTERS.SERVICES}/${product.type}/${service.id}/${service.name}`,
-            });
-          })
-        }
-      })
-      setProducts(productList);
-      setGlobalStore({
-        products: productList
-      });
-    })
-  }
-  const getPosts = () => {
-    PostService.getPosts(response => {
-      const postList = response.posts.map(post => {
-        return ({
-          ...post,
-          key: `${ROUTERS.POSTS}/${post.key}/${post.name}`,
-        });
-      })
-      setPosts(postList);
-    })
-  }
-  useEffect(() => {
-    getProducts();
-    getPosts();
-    // eslint-disable-next-line
-  }, []);
-  const productsItems = (products || []).map(product => {
-    const servicesItems = product.services.map(service => {
-      return getItem(service.name, service.key, <YoutubeOutlined />);
-    })
-    return getItem(product.name.toUpperCase(), product.key, undefined, servicesItems);
-  })
-  const postsItem = !!posts ? getItem('BÀI VIẾT', 'posts', undefined, posts.map(post => {
-    return getItem(post.name, post.key);
-  })) : {};
   const items = [
-      ...productsItems,
-    getItem('CHO THÀNH VIÊN', 'customer', undefined, [
-      getItem('Lịch sử đơn hàng', ROUTERS.ORDERS_HISTORY, <OrderedListOutlined />),
-      getItem( 'Lịch sử đơn nạp', ROUTERS.INVOICES_HISTORY, <SwapOutlined />),
-      getItem('Bảng giá Cộng tác viên', ROUTERS.PRICES_FOR_PARTNER, <TableOutlined />),
-      getItem('Tìm ID facebook', ROUTERS.FIND_FACEBOOK_ID, <LinkOutlined />),
-      getItem('Kiếm tiền', ROUTERS.MAKE_MONEY, <DollarOutlined />),
-      getItem('Thông báo', ROUTERS.NOTIFICATION, <BellOutlined />),
-    ]),
-    getItem('LIÊN HỆ', 'contacts', undefined, [
-      getItem(`Fanpage ${WEBSITE_DOMAIN}`, ROUTERS.FB_PAGE, <FacebookOutlined />),
-      getItem( `Admin ${WEBSITE_DOMAIN}`, ROUTERS.FB_ADMIN, <UserOutlined />),
-      getItem('Liên hệ', ROUTERS.CONTACT_INFO, <ContactsOutlined />),
-    ]),
-    postsItem,
+    getItem('Trang chủ', ROUTERS.ROOT),
   ];
   const defaultOpenKeys = items.map(item => item.key);
 
   const onClick = (e) => {
-    switch (e.key) {
-      case ROUTERS.FB_PAGE:
-        window.open('https://www.facebook.com/FanpageLike68/?ref=website', '_blank');
-        break;
-      case ROUTERS.FB_ADMIN:
-        window.open('https://www.facebook.com/mr.QuocDoan','_blank');
-        break;
-      default:
-        redirectTo(e.key);
-    }
+    redirectTo(e.key);
   };
   return (
-    !!products && !!posts ? <Sider items={items}
+    <Sider items={items}
            defaultOpenKeys={defaultOpenKeys}
            defaultSelectedKeys={selectedRouters}
            onClick={onClick}
-    /> : ''
+           mode="inline"
+    />
   );
 }
