7ec8606fde8aadf329902dddd266b46ae557f8b2
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-02 17:49:47 +0700

    Update AdminSider.js

--- src/components/Admin/AdminSider.js
+++ src/components/Admin/AdminSider.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { UserOutlined, ClusterOutlined, OrderedListOutlined, ProfileOutlined, SwapOutlined } from '@ant-design/icons';
+import { OrderedListOutlined, ProfileOutlined, SwapOutlined } from '@ant-design/icons';
 import Sider, { getItem } from 'components/Share/Layout/Sider';
 
 export default function AdminSider(props) {
   const items = [
-    getItem('TÀI KHOẢN & PHÂN QUYỀN', 'facebook', undefined, [
-      getItem('Quản lý tài khoản', 'fb1', <UserOutlined />),
-      getItem('Quản lý quyển', 'fb2', <ClusterOutlined />),
-    ]),
-    getItem('ĐƠN HÀNG & SẢN PHẨM && ĐƠN NẠP', 'tiktok', undefined, [
-      getItem('Quản lý đơn hàng', 'tt1', <OrderedListOutlined />),
-      getItem('Quản lý sản phẩm', 'tt2', <ProfileOutlined />),
-      getItem('Quản lý đơn nạp', 'tt3', <SwapOutlined />),
-    ]),
+    getItem('Quản lý đơn hàng', 'tt1', <OrderedListOutlined />),
+    getItem('Quản lý sản phẩm', 'tt2', <ProfileOutlined />),
+    getItem('Quản lý đơn nạp', 'tt3', <SwapOutlined />),
   ];
   const defaultOpenKeys = ['facebook', 'tiktok']
   const defaultSelectedKeys = ['fb1']
@@ -25,6 +19,7 @@ export default function AdminSider(props) {
            defaultOpenKeys={defaultOpenKeys}
            defaultSelectedKeys={defaultSelectedKeys}
            onClick={onClick}
+           mode="inline"
     />
   );
 }
