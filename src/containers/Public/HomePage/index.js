787b9b77df41da6363141d32e6b0daa63c04dfc0
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-02 17:58:46 +0700

    Create index.js

---
+++ src/containers/Public/HomePage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+export default function HomePage() {
+  return (
+    <div>
+      <Helmet>
+        <title>Admins Page</title>
+      </Helmet>
+      Admins Page
+    </div>
+  );
+}
