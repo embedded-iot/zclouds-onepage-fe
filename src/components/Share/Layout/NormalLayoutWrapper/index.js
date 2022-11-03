a6d2eaca8f60d932cdd74cdf87b4c2b079f058f9
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-03 17:59:50 +0700

    Create index.js

---
+++ src/components/Share/Layout/NormalLayoutWrapper/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Layout } from 'antd';
+import PropTypes from 'prop-types';
+import './style.scss';
+
+const { Header, Content } = Layout;
+
+
+const NormalLayoutWrapper = props => {
+  return (
+    <Layout className="normal-layout__wrapper">
+      <Header>
+        {props.header}
+      </Header>
+      <Content>
+        {props.content}
+      </Content>
+    </Layout>
+  );
+}
+
+NormalLayoutWrapper.propTypes = {
+  header: PropTypes.element,
+  content: PropTypes.element,
+};
+
+export default NormalLayoutWrapper;
