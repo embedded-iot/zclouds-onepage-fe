46f0b8e84f5ae443dc581bdd98bfbe1b76959827
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-03 17:57:13 +0700

    Update index.js

--- src/components/FrontUser/Header/index.js
+++ src/components/FrontUser/Header/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import UserActions from 'components/FrontUser/UserActions';
+import { WEBSITE_NAME } from 'components/contants';
+import logo from 'images/logo.svg';
 
 import './style.scss';
 
@@ -7,7 +9,9 @@ export default function PublicHeader({ logoName, sider, redirectTo }) {
   return (
     <div className="header__wrapper">
       <div className='logo-portal'>
-        <a href='/Outsource/react-app/public'>{ logoName }</a>
+        <a href='/'>
+          <img src={logo} alt={WEBSITE_NAME} />
+        </a>
       </div>
       <div className="header__actions">
         <div className="header__menu">{ sider }</div>
