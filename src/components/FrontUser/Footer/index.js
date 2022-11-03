508e65a4103c728087096e9693f389c36c3ed179
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-03 17:56:38 +0700

    Update index.js

--- src/components/FrontUser/Footer/index.js
+++ src/components/FrontUser/Footer/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { WEBSITE_DOMAIN } from 'components/contants';
 
-import 'components/FrontUser/Footer/style.scss';
-import { Divider } from 'antd';
+import './style.scss';
 
 export default function Footer() {
   return (
     <div className="footer-wrapper">
       <div className='footer-contents'>
-        <div>Hệ thống hoạt động 24/24. Nhân viên hỗ trợ 09-18h từ T2-T7</div>
-        <Divider/>
         <div>Dev by {WEBSITE_DOMAIN} | Điều khoản & Quy định | Chính sách bảo mật</div>
       </div>
     </div>
