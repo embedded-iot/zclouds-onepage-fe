4f6ec621e03a802273c76f4ab9d31e62550e4fe9
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-04 18:00:29 +0700

    LoginForm

--- src/components/Share/LoginForm/index.js
+++ src/components/Share/LoginForm/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Button, Card, Form, Input } from 'antd';
-import { WEBSITE_NAME } from 'components/contants';
+import BoxHeader from 'components/Share/BoxHeader';
 
 export default function LoginForm({ onFinish = () => {}, redirectTo = () => {} }) {
   return (
     <Card>
+      <BoxHeader
+        title="Sign in to Creative Space"
+        description="Log in with your data that you entered during registration."
+      />
       <Form
         name="basic"
         onFinish={onFinish}
@@ -12,43 +16,36 @@ export default function LoginForm({ onFinish = () => {}, redirectTo = () => {} }
         layout="vertical"
       >
         <Form.Item
-          label="Tên đăng nhập"
-          name="loginId"
+          name="userName"
           rules={[
             {
               required: true,
-              message: 'Vui lòng nhập tên đăng nhập của bạn!',
+              message: 'Please enter user name!',
             },
           ]}
         >
-          <Input placeholder={`Tên đăng nhập ${WEBSITE_NAME}`}  />
+          <Input placeholder="User Name"  />
         </Form.Item>
 
         <Form.Item
-          label="Mật khẩu"
           name="password"
           rules={[
             {
               required: true,
-              message: 'Vui lòng nhập mật khẩu đăng nhập của bạn!',
+              message: 'Please enter password!',
             },
           ]}
         >
-          <Input.Password placeholder={`Mật khẩu ${WEBSITE_NAME}`} />
+          <Input.Password placeholder="Password" />
         </Form.Item>
         <Form.Item>
           <Button type="primary" size='large' htmlType="submit">
-            Đăng nhập
+            Log in
           </Button>
         </Form.Item>
         <Form.Item>
-          <div>
-            Quên mật khẩu?
-            <Button type="link" danger onClick={() => redirectTo("/forgot-account")}>Lấy lại tại đây</Button>
-          </div>
-          <div>
-            Nếu bạn đã chưa có tài khoản,
-            <Button type="link" danger onClick={() => redirectTo("/register")}>Đăng ký tại đây</Button>
+          <div style={{ textAlign: 'center'}}>
+            Don't have an account? <a onClick={() => redirectTo("/register")}>Sign up</a>.
           </div>
         </Form.Item>
       </Form>
