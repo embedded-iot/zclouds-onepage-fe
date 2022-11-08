4ddd0de1e8c62817d933f972af242d1a4c43e0ee
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-08 17:50:47 +0700

    UserInfo

--- src/components/Share/UserInfo/index.js
+++ src/components/Share/UserInfo/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import { Avatar, Button, Dropdown, Menu } from 'antd';
+import { Avatar, Badge, Dropdown, Menu } from 'antd';
 import {
-  LoginOutlined,
-  DollarOutlined,
-  UserAddOutlined,
   LogoutOutlined,
   UserOutlined,
-  OrderedListOutlined, SwapOutlined,
+  BellOutlined,
 } from '@ant-design/icons';
-import { RESPONSIVE_MEDIAS, ROUTERS } from 'components/contants';
-import { format } from 'utils';
-import { useMediaQuery } from 'react-responsive';
+import { ROUTERS } from 'components/contants';
 
 import './style.scss';
 
 export default function UserInfo({ isLogin = false, isAdmin = false, currentUser = {}, redirectTo = () => {}, signOut = () => {}}) {
-  const isMobile = useMediaQuery(RESPONSIVE_MEDIAS.MOBILE);
   const handleMenuClick = (e) => {
     switch (e.key) {
       case ROUTERS.LOGOUT:
@@ -27,19 +21,6 @@ export default function UserInfo({ isLogin = false, isAdmin = false, currentUser
     }
   }
 
-  const userMenuItems = isLogin && !isAdmin ? [
-    {
-      label: 'Lịch sử đơn hàng',
-      key: ROUTERS.ACCOUNT_ASSETS_ORDERS_HISTORY,
-      icon: <OrderedListOutlined />,
-    },
-    {
-      label: 'Lịch sử đơn nạp',
-      key: ROUTERS.ACCOUNT_ASSETS_INVOICES_HISTORY,
-      icon: <SwapOutlined />,
-    }
-  ] : [];
-
   const menu = (
     <Menu
       onClick={handleMenuClick}
@@ -49,7 +30,6 @@ export default function UserInfo({ isLogin = false, isAdmin = false, currentUser
           key: ROUTERS.ACCOUNT_INFO,
           icon: <UserOutlined />,
         },
-        ...userMenuItems,
         {
           label: 'Đăng xuất',
           key: ROUTERS.LOGOUT,
@@ -60,52 +40,32 @@ export default function UserInfo({ isLogin = false, isAdmin = false, currentUser
   );
 
   return (
-    <div className="user-info-wrapper">
-      { !isLogin && (
-        <>
-          <Button type="primary" danger icon={<DollarOutlined />} onClick={() => redirectTo(ROUTERS.PRICES)}>{!isMobile && 'Bảng giá' }</Button>
-          <Button icon={<UserAddOutlined />} onClick={() => redirectTo(ROUTERS.REGISTER)}>{!isMobile && 'Đăng ký' }</Button>
-          <Button type="primary" icon={<LoginOutlined />} onClick={() => redirectTo(ROUTERS.LOGIN)}>{!isMobile && 'Đăng nhập' }</Button>
-        </>
-      )}
-      { isLogin && (
-        <>
-          {
-            !isMobile ? (
-              <>
-                <span>Xin chào: <b>{currentUser.name || 'USER'}</b></span>
-                <span>Số dư: </span>
-                <Button type="primary" size="small" danger>
-                  {format.formatCurrency(currentUser.credit)}
-                </Button>
-              </>
-            ) : (
-              <>
-                <Button type="primary" size="small" danger>
-                  {format.formatCurrency(currentUser.credit)}
-                </Button>
-              </>
-            )
-          }
-          <Button type="primary" size="small" icon={<DollarOutlined />} onClick={() => redirectTo(ROUTERS.ACCOUNT_ASSETS_INVOICES_METHODS)}>Nạp tiền</Button>
-          <Dropdown
-            overlay={menu}
-            placement="bottomRight"
-            arrow={{
-              pointAtCenter: true,
+    <div className="user-info__wrapper">
+      <Badge count={5}>
+        <BellOutlined style={{ fontSize: 25 }} />
+      </Badge>
+      <Dropdown
+        overlay={menu}
+        placement="bottomRight"
+        arrow={{
+          pointAtCenter: true,
+        }}
+      >
+        <div className="user-info__avatar">
+          <Avatar
+            style={{
+              backgroundColor: '#87d068',
+              cursor: 'pointer'
             }}
-          >
-            <Avatar
-              style={{
-                backgroundColor: '#87d068',
-                cursor: 'pointer'
-              }}
-              src={currentUser.avatar}
-              icon={<UserOutlined />}
-            />
-          </Dropdown>
-        </>
-      )}
+            src={currentUser.avatar}
+            icon={<UserOutlined />}
+          />
+          <div className="user-info__title">
+            <span>{currentUser.name || 'User name'}</span>
+            <span className="link">{currentUser.role || (isAdmin ? 'Admin' : 'Seller')}</span>
+          </div>
+        </div>
+      </Dropdown>
     </div>
   );
 }
