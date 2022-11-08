81397a359a15d119cfb1ca9fcd873cd04d766fff
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-08 17:43:12 +0700

    Update contants.js

--- src/components/contants.js
+++ src/components/contants.js
@@ -3,58 +3,74 @@ export const WEBSITE_DOMAIN = 'Fulfil.com';
 export const ROUTERS = {
   ROOT: '/',
   LOGIN: '/login',
-  REGISTER: '/register',
   LOGOUT: '/logout',
+  FRONT_USER_REGISTER: '/register',
   FRONT_USER_ALL_PRODUCTS: '/categories',
   FRONT_USER_SKU: '/sku',
-  ACCOUNT_INFO: '/account-info',
-  SERVICES: `/services`,
-  DETAIL_SERVICE: `/services/:productType/:serviceId/:serviceName`,
-  ACCOUNT_ASSETS: '/account-assets/:tab',
-  ACCOUNT_ASSETS_INVOICES_HISTORY: '/account-assets/deposits-history',
-  ACCOUNT_ASSETS_INVOICES_METHODS: '/account-assets/deposit-methods',
-  ACCOUNT_ASSETS_ORDERS_HISTORY: '/account-assets/orders-history',
-  INVOICES_HISTORY: '/deposits-history',
-  ORDERS_HISTORY: '/orders-history',
-  PRICES: '/prices',
-  PRICES_FOR_PARTNER: '/prices-for-partner',
-  FIND_FACEBOOK_ID: '/find-facebook-id',
-  MAKE_MONEY: '/make-money',
-  NOTIFICATION: '/notification',
-  FB_PAGE: '/fb-page',
-  FB_ADMIN: '/fb-admin',
-  CONTACT_INFO: '/contact-info',
-  POSTS: '/posts',
-  DETAIL_POSTS: '/posts/:postKey/:postName',
+  SELLER_ORDERS: '/orders',
+  SELLER_DESIGN_LIBRARY: '/design-library',
+  SELLER_MY_ACCOUNT: '/my-account',
+  SELLER_STORES: '/stores',
+  SELLER_WALLET: '/wallet',
+  SELLER_INTEGRATIONS: '/integration',
+  SELLER_PRODUCT_CATEGORY: '/product-category',
+  SELLER_TICKETS: '/tickets',
+  SELLER_CHAT_WITH_ME: '/chat-with-me',
+  SELLER_CREATOR_COMMUNITY: '/creator-community',
+  ADMIN_PRODUCTS_MANAGEMENT: '/products-management',
+  ADMIN_ADMINS_AND_ROLES_MANAGEMENT: '/admins-and-roles-management',
+  ADMIN_ADMINS_MANAGEMENT: '/admins-management',
+  ADMIN_ROLES_MANAGEMENT: '/roles-management',
 }
 
-export const ORDER_STATUS = {
-  REQUESTED: 'REQUESTED',
-  IN_PROGRESS: 'IN_PROGRESS',
-  DONE: 'DONE',
-  REJECTED: 'REJECTED',
-  CANCELED: 'CANCELED',
+export const SHIPPING_EXPRESSES = {
+  YUN_EXPRESS: 'Yun Express',
+  YUN_FAST: 'Funfast',
+  FED_EX: 'FedEx',
+  DHL: 'DHL',
 };
 
-export const ORDER_STATUS_LABEL = {
-  [ORDER_STATUS.REQUESTED]: 'Đã gửi yêu cầu',
-  [ORDER_STATUS.IN_PROGRESS]: 'Đang xử lý...',
-  [ORDER_STATUS.DONE]: 'Hoàn thành',
-  [ORDER_STATUS.REJECTED]: 'Đã từ chối',
-  [ORDER_STATUS.CANCELED]: 'Đã hủy',
-}
+export const SHIPPING_EXPRESSES_VALUES_OPTIONS = [
+  SHIPPING_EXPRESSES.YUN_EXPRESS,
+  SHIPPING_EXPRESSES.YUN_FAST,
+  SHIPPING_EXPRESSES.FED_EX,
+  SHIPPING_EXPRESSES.DHL,
+]
 
 
-export const ORDER_STATUS_OPTIONS = [
-  { label: ORDER_STATUS_LABEL[ORDER_STATUS.REQUESTED], value: ORDER_STATUS.REQUESTED },
-  { label: ORDER_STATUS_LABEL[ORDER_STATUS.IN_PROGRESS], value: ORDER_STATUS.IN_PROGRESS },
-  { label: ORDER_STATUS_LABEL[ORDER_STATUS.DONE], value: ORDER_STATUS.DONE },
-  { label: ORDER_STATUS_LABEL[ORDER_STATUS.REJECTED], value: ORDER_STATUS.REJECTED },
-  { label: ORDER_STATUS_LABEL[ORDER_STATUS.CANCELED], value: ORDER_STATUS.CANCELED },
+export const SHIPPING_EXPRESSES_LABEL_VALUE_OPTIONS = [
+  { label: SHIPPING_EXPRESSES.YUN_EXPRESS, value: SHIPPING_EXPRESSES.YUN_EXPRESS },
+  { label: SHIPPING_EXPRESSES.YUN_FAST, value: SHIPPING_EXPRESSES.YUN_FAST },
+  { label: SHIPPING_EXPRESSES.FED_EX, value: SHIPPING_EXPRESSES.FED_EX },
+  { label: SHIPPING_EXPRESSES.DHL, value: SHIPPING_EXPRESSES.DHL },
+];
+
+
+export const SIZES = {
+  _S: 'S',
+  _M: 'M',
+  _L: 'L',
+  _XL: 'XL',
+  _2XL: '2XL',
+};
+
+export const SIZES_VALUES_OPTIONS = [
+  SIZES._S,
+  SIZES._M,
+  SIZES._L,
+  SIZES._XL,
+  SIZES._2XL,
+]
+
+export const SIZES_LABEL_VALUE_OPTIONS = [
+  { label: SIZES._S, value: SIZES._S },
+  { label: SIZES._M, value: SIZES._M },
+  { label: SIZES._L, value: SIZES._L },
+  { label: SIZES._XL, value: SIZES._XL },
+  { label: SIZES._2XL, value: SIZES._2XL },
 ];
 
 export const DATETIME_FORMAT = "DD/MM/YYYY HH:MM";
-
 
 export const RESPONSIVE_MEDIAS = {
   MOBILE: { query: '(max-width: 768px)' },
