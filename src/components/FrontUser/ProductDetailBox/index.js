28c8b0ec22ddf82f4ebd7e133552ef0ad97a021f
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-09 17:53:02 +0700

    ProductDetailBox

---
+++ src/components/FrontUser/ProductDetailBox/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Col, Row } from 'antd';
+
+export default function ProductDetailBox({ category, productName }) {
+  return (
+    <div className='product-detail-box__wrapper'>
+      <Row gutter={[65, 0]}>
+        <Col span={12}>
+          ádas
+        </Col>
+        <Col span={12}>
+          đá
+        </Col>
+      </Row>
+    </div>
+  );
+}
