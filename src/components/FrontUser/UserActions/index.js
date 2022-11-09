99759b4c64ef8da98e6f4fc69cb1de7869be0c8d
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-09 17:54:00 +0700

    UserActions

--- src/components/FrontUser/UserActions/index.js
+++ src/components/FrontUser/UserActions/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button } from 'antd';
 import {
   LoginOutlined,
-  UserAddOutlined,
+  UserOutlined,
 } from '@ant-design/icons';
 import { RESPONSIVE_MEDIAS, ROUTERS } from 'components/contants';
 import { useMediaQuery } from 'react-responsive';
@@ -13,8 +13,8 @@ export default function UserActions({ redirectTo = () => {}}) {
   const isMobile = useMediaQuery(RESPONSIVE_MEDIAS.MOBILE);
   return (
     <div className={`user-action__wrapper ${!!isMobile && 'user-action__wrapper--mobile'}`} >
-      <Button icon={<UserAddOutlined />} onClick={() => redirectTo(ROUTERS.REGISTER)}>Sign Up</Button>
-      <Button type={'primary'} icon={<LoginOutlined />} onClick={() => redirectTo(ROUTERS.LOGIN)}>Log in</Button>
+      <Button icon={<LoginOutlined />} onClick={() => redirectTo(ROUTERS.REGISTER)}>Sign Up</Button>
+      <Button icon={<UserOutlined />} onClick={() => redirectTo(ROUTERS.LOGIN)}>Log in</Button>
     </div>
   );
 }
