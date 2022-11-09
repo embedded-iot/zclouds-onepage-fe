1f6ea53140f808e4d5139fc2cdde93c8f12a56b1
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-09 17:51:02 +0700

    CategoriesFilters

--- src/components/FrontUser/CategoriesGrid/CategoriesFilters/index.js
+++ src/components/FrontUser/CategoriesGrid/CategoriesFilters/index.js
@@ -2,16 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { FrontUserCategoriesService } from 'services';
 import "./style.scss";
 
-const items = [
-  { label: 'All products', count: 293, value: '' },
-  { label: 'Apparel', count: 12, value: 'Apparel' },
-  { label: 'Gift & Accessories38', count: 21, value: 'Gift & Accessories38' },
-  { label: 'Home & Decorations', count: 23, value: 'All Over Print' },
-  { label: 'Canvas & Poster', count: 23, value: 'Canvas & Poster' },
-  { label: 'Shoes', count: 2, value: 'Shoes' },
-  { label: 'US 2D Printing', count: 23, value: 'US 2D Printing' },
-]
-
 export default function CategoriesFilters({ onChange = () => {} }) {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('');
@@ -19,8 +9,6 @@ export default function CategoriesFilters({ onChange = () => {} }) {
   const getCategoriesFilter = () => {
     FrontUserCategoriesService.getCategoriesFilter(response => {
       setCategories(response);
-    }, error => {
-      setCategories(items);
     })
   }
 
@@ -45,8 +33,8 @@ export default function CategoriesFilters({ onChange = () => {} }) {
                  key={index}
                  onClick={() => onSelectFilter(category.value)}
             >
-              <div className='categories-filters__label'>{category.label}</div>
-              <div className='categories-filters__count'>{category.count}</div>
+              <div className='categories-filters__item-label'>{category.label}</div>
+              <div className='categories-filters__item-count'>{category.count}</div>
             </div>
           ))
         }
