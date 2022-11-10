db0195e6f2b9086cb4c78227e723322c1da24b51
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-10 17:51:11 +0700

    Update index.js

--- src/components/FrontUser/ProductDetailBox/index.js
+++ src/components/FrontUser/ProductDetailBox/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'antd';
+import { FrontUserCategoriesService } from 'services';
+import ProductImagesPreview from './ProductImagesPreview';
 
-export default function ProductDetailBox({ category, productName }) {
+export default function ProductDetailBox({ productId, productName }) {
+  const [product, setProduct] = useState(null);
+
+  const getProductDetail = () => {
+    FrontUserCategoriesService.getProductDetail(productId, response => {
+      setProduct(response);
+    })
+  }
+  useEffect(() => {
+    getProductDetail();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className='product-detail-box__wrapper'>
       <Row gutter={[65, 0]}>
         <Col span={12}>
-          ádas
+          <ProductImagesPreview product={product}/>
         </Col>
         <Col span={12}>
           đá
