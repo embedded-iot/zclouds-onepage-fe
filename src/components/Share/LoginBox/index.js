1819e11a8ca81be2e50ea7cd3b7f507782eb117b
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-10 17:53:09 +0700

    Update index.js

--- src/components/Share/LoginBox/index.js
+++ src/components/Share/LoginBox/index.js
@@ -3,13 +3,14 @@ import { UserService } from 'services';
 import { notification } from 'antd';
 import { authentication } from 'utils';
 import LoginForm from 'components/Share/LoginForm';
+import { ADMIN_ROLES } from 'components/contants';
 
 export default function LoginBox({ setGlobalStore = () => {}, redirectTo = () => {}, onFinish = () => {}}) {
   const getUserInfo = (callback) => {
     UserService.getUserInfo(response => {
       setGlobalStore({
         isLogin: true,
-        isAdmin: false,
+        isAdmin: ADMIN_ROLES.includes(response.role),
         currentUser: {
           ...response
         }
@@ -17,32 +18,22 @@ export default function LoginBox({ setGlobalStore = () => {}, redirectTo = () =>
       callback();
     }, error => {
       notification.error({
-        message: error.title || "Không thể lấy thông tin tài khoản bây giờ. Vui lòng thử lại sau!",
+        message: error && error.title ? error.title : "Login failure!",
       });
     })
   }
   const handlerFinish = (values) => {
-    setGlobalStore({
-      isLogin: true,
-      isAdmin: process.env.REACT_APP_ADMIN_MODE === 'true',
-    })
-    notification.success({
-      message: "Đăng nhập thành công!",
-    });
-    onFinish();
-    return;
-    // eslint-disable-next-line
     UserService.login(values, response => {
-      authentication.setToken(response.id_token);
+      authentication.setToken(response.token);
       getUserInfo(() => {
         notification.success({
-          message: "Đăng nhập thành công!",
+          message: "Login successful!",
         });
         onFinish();
       })
     }, error => {
       notification.error({
-        message: error.title || "Tên đăng nhập hoặc mật khẩu không đúng."
+        message: error && error.title ? error.title : "Login failure!",
       });
     });
   }
