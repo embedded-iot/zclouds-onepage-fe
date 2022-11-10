3eb87e4b604fb410ac0063446c36c5a8938ec361
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-10 17:52:53 +0700

    UserActions

--- src/components/FrontUser/UserActions/index.js
+++ src/components/FrontUser/UserActions/index.js
@@ -13,7 +13,7 @@ export default function UserActions({ redirectTo = () => {}}) {
   const isMobile = useMediaQuery(RESPONSIVE_MEDIAS.MOBILE);
   return (
     <div className={`user-action__wrapper ${!!isMobile && 'user-action__wrapper--mobile'}`} >
-      <Button icon={<LoginOutlined />} onClick={() => redirectTo(ROUTERS.REGISTER)}>Sign Up</Button>
+      <Button icon={<LoginOutlined />} onClick={() => redirectTo(ROUTERS.FRONT_USER_REGISTER)}>Sign Up</Button>
       <Button icon={<UserOutlined />} onClick={() => redirectTo(ROUTERS.LOGIN)}>Log in</Button>
     </div>
   );
