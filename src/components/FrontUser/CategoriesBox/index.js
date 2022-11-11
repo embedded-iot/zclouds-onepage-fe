920ba41bd0accd30b517bc5472e0b6960e936701
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-11 16:56:51 +0700

    CategoriesBox

---
+++ src/components/FrontUser/CategoriesBox/index.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import MultiCarouselView from 'components/Common/MultiCarouselView';
+import { FrontUserCategoriesService } from 'services';
+import { ROUTERS } from 'components/contants';
+import CategoryCard from 'components/FrontUser/CategoryCard';
+
+export default function CategoriesBox({ redirectTo }) {
+  const [categories, setCategories] = useState([]);
+  const getCategories = () => {
+    FrontUserCategoriesService.getCategoriesFilter(response => {
+      console.log(response);
+      setCategories(response);
+    })
+  }
+
+  useEffect(() => {
+    getCategories();
+    // eslint-disable-next-line
+  }, []);
+
+  const handleClick = category => {
+    const { categoryName, categoryId } = category;
+    redirectTo(ROUTERS.FRONT_USER_ALL_PRODUCTS + `/${categoryName}/${categoryId}`);
+  }
+  return !!categories.length ? (
+    <MultiCarouselView
+      deviceType="desktop"
+    >
+      {
+        // categories.map((category) => <CategoryCard {...category} onClick={handleClick} />)
+        categories.map((category, index) => (<div key={index}>2323</div>))
+      }
+    </MultiCarouselView>
+  ) : null;
+}
