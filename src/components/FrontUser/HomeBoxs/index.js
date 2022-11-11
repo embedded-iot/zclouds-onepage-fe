51dfc58a62cfda817485fbb32178e5bec4caaa13
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-11 17:05:35 +0700

    Create index.js

---
+++ src/components/FrontUser/HomeBoxs/index.js
@@ -0,0 +1,13 @@
+import BannerBox from './BannerBox';
+import BannerBox1 from './BannerBox1';
+import BannerBox2 from './BannerBox2';
+import BannerBox3 from './BannerBox3';
+
+import './style.scss';
+
+export {
+  BannerBox,
+  BannerBox1,
+  BannerBox2,
+  BannerBox3,
+}
