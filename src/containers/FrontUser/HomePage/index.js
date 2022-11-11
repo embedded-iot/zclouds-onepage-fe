3644fc3b0049fd0d82b61f697803f26608e0edf5
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-11 17:07:13 +0700

    Update index.js

--- src/containers/FrontUser/HomePage/index.js
+++ src/containers/FrontUser/HomePage/index.js
@@ -1,15 +1,62 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { BannerBox, BannerBox1, BannerBox2, BannerBox3 } from 'components/FrontUser/HomeBoxs';
+import { push } from 'connected-react-router';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { useMediaQuery } from 'react-responsive';
+import { RESPONSIVE_MEDIAS } from 'components/contants';
 
-export default function HomePage(props) {
+function HomePage(props) {
+  const isDesktop = useMediaQuery(RESPONSIVE_MEDIAS.DESKTOP);
+  const isTablet = useMediaQuery(RESPONSIVE_MEDIAS.TABLET);
+  // eslint-disable-next-line
+  const customClass = !!isTablet && 'home-box__wrapper--tablet' || !!isDesktop && 'home-box__wrapper--desktop';
+
   return (
     <div className="page-wrapper--full-width">
       <Helmet>
         <title>Home</title>
       </Helmet>
       <div className="page-contents">
-        Home page
+        <BannerBox
+          customClass={customClass}
+          redirectTo={props.push}
+        />
+        <BannerBox1
+          customClass={customClass}
+          redirectTo={props.push}
+        />
+        <BannerBox2
+          customClass={customClass}
+          redirectTo={props.push}
+        />
+        <BannerBox3
+          customClass={customClass}
+          redirectTo={props.push}
+        />
       </div>
     </div>
   );
 }
+
+function mapStateToProps(state) {
+  return {
+    router: state.router,
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    push: path => dispatch(push(path)),
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+export default compose(
+  withConnect,
+)(HomePage);
