9af19756b62c719bdcbaa52fac029e2f9dc06dd8
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-14 17:43:23 +0700

    Update index.js

--- src/components/Admin/ProductsManagementTable/DeleteProductModal/index.js
+++ src/components/Admin/ProductsManagementTable/DeleteProductModal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ModalView, { MODAL_TYPES } from 'components/Common/ModalView';
-import { AdminProductsService } from 'services';
+import { AdminProductsService, BaseService } from 'services';
 import { notification } from 'antd';
 
 export default function DeleteProductModal({ open, data, onOk, onCancel }) {
@@ -12,7 +12,7 @@ export default function DeleteProductModal({ open, data, onOk, onCancel }) {
       onOk();
     }, error => {
       notification.error({
-        message: error && error.title ? error.title : "Delete product failure!",
+        message: BaseService.getErrorMessage(error,"Delete product failure!"),
       });
     })
   }
