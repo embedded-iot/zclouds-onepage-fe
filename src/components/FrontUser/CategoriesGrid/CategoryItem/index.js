fe75db15467fc7cf2cf67c0442bbe56b58d99051
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-15 17:47:20 +0700

    Update index.js

--- src/components/FrontUser/CategoriesGrid/CategoryItem/index.js
+++ src/components/FrontUser/CategoriesGrid/CategoryItem/index.js
@@ -3,18 +3,18 @@ import { Card } from 'antd';
 
 import './style.scss';
 
-export default function CategoryItem({ showTitle = true, showDes1 = true, showDes2 = true, onClick = () => {}, ...restProp}) {
-  const { id, name, avatar, price, sizes = 0, colors = 0, print = 0} = restProp || {};
+export default function CategoryItem({ showTitle = true, showDes1 = true, showDes2 = true, onClick = () => {}, imgProps = {}, ...restProp}) {
+  const { id, name, avatar, convertedPrice, productOptionsLabel } = restProp || {};
   return (
     <Card
       className="category-item__wrapper"
-      cover={<img alt={name} src={avatar} />}
+      cover={<img alt={name} src={avatar} {...imgProps} />}
       onClick={() => onClick(restProp)}
       key={id}
     >
       { showTitle && <div className='category-item__title'>{name}</div>}
-      { showDes1 && <div className='category-item__description-1'>From: {price}</div>}
-      { showDes2 && <div className='category-item__description-2'>{`${sizes} sizes ${colors} colors ${print} print providers`}</div>}
+      { showDes1 && <div className='category-item__description-1'>From: {convertedPrice}</div>}
+      { showDes2 && <div className='category-item__description-2'>{ !!productOptionsLabel && `${productOptionsLabel} providers`}</div>}
     </Card>
   )
 }
