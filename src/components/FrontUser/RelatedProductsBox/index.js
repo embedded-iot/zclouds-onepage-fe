80cf7a4f90941c65dff04f61f50332bd02bb3686
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-15 17:49:18 +0700

    Update index.js

--- src/components/FrontUser/RelatedProductsBox/index.js
+++ src/components/FrontUser/RelatedProductsBox/index.js
@@ -7,7 +7,7 @@ import { ROUTERS } from 'components/contants';
 export default function RelatedProductsBox({ categoryId, redirectTo }) {
   const [products, setProducts] = useState([]);
   const getProductsByCategoryId = () => {
-    FrontUserCategoriesService.getCategories({ categoryId, pageNum: 1, pageSize: 10000 }, response => {
+    FrontUserCategoriesService.getCategories({ categoryId }, response => {
       setProducts(response.items);
     })
   }
@@ -21,12 +21,13 @@ export default function RelatedProductsBox({ categoryId, redirectTo }) {
     const { categoryName, categoryId, productName, productId } = product;
     redirectTo(ROUTERS.FRONT_USER_ALL_PRODUCTS + `/${categoryName}/${productName}/${categoryId}/${productId}`);
   }
+
   return (
     <MultiCarouselView
       deviceType="desktop"
     >
       {
-        products.map((product) => <CategoryItem {...product} onClick={handleClick} showDes2={false}/>)
+        products.map((product) => <CategoryItem {...product} imgProps={{ style: {minHeight: 250} }} onClick={handleClick} showDes2={false}/>)
       }
     </MultiCarouselView>
   );
