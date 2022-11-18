79a36497734e953e331a91d3f3328f6acb618284
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-18 17:44:46 +0700

    Update index.js

--- src/components/FrontUser/HomeBoxs/index.js
+++ src/components/FrontUser/HomeBoxs/index.js
@@ -4,6 +4,7 @@ import BannerBox2 from './BannerBox2';
 import BannerBox3 from './BannerBox3';
 import BannerBox4 from './BannerBox4';
 import BannerBox5 from './BannerBox5';
+import BannerBox6 from './BannerBox6';
 
 import './style.scss';
 
@@ -14,4 +15,5 @@ export {
   BannerBox3,
   BannerBox4,
   BannerBox5,
+  BannerBox6,
 }
