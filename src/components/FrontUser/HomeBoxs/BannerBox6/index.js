e519b18d07e775aafd3f4d43473639335a4f566f
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-18 17:44:13 +0700

    Update index.js

--- src/components/FrontUser/HomeBoxs/BannerBox6/index.js
+++ src/components/FrontUser/HomeBoxs/BannerBox6/index.js
@@ -1,32 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, Col, Row } from 'antd';
-import CategoriesSlideBox from 'components/FrontUser/CategoriesSlideBox';
-import CategorySlideItem from 'components/FrontUser/CategorySlideItem';
+import BlogsSlideBox from 'components/FrontUser/BlogsSlideBox';
+import SignUpBannerBox from 'components/FrontUser/SignUpBannerBox';
 
 import './style.scss';
 
-export default function BannerBox3({ customClass, redirectTo }) {
-  const [category, setCategory] = useState({});
-
-  const getCategoriesCallback = (categories = []) => {
-    if (categories.length === 0) return;
-    setCategory(categories[0])
-  }
+export default function BannerBox6({ customClass, redirectTo }) {
   return (
-    <div className={`home-box__wrapper ${customClass} banner-box-3__wrapper`}>
+    <div className={`home-box__wrapper ${customClass} banner-box-6__wrapper`}>
       <Row>
-        <Col span={24} className="banner-box-3__center-box">
-          <div className='banner-box-3__contents'>
+        <Col span={24} className="banner-box-6__center-box">
+          <div className='banner-box-6__contents'>
             <div className='home-box__header-text'>
               LATEST CONTENT
             </div>
             <div className='home-box__title'>
-              <span className="link link--text">Blogs</span> And News
+              <span className="link link--text banner-box-6__link">Blogs</span> And News
             </div>
             <div className='home-box__description'>
               Quickly connect with your store. You can even integrate Lenful with your management system using the API.
             </div>
-
+            <div className='banner-box-6__blog-list'>
+              <BlogsSlideBox
+                redirectTo={redirectTo}
+              />
+            </div>
+            <div className='banner-box-6__buttons'>
+              <Button>See more posts</Button>
+            </div>
+            <SignUpBannerBox />
           </div>
         </Col>
       </Row>
