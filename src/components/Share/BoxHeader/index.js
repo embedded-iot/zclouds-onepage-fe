f5dbb52dcb28e9586c1121e3f277b7f8cc87e600
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-18 17:46:13 +0700

    Update index.js

--- src/components/Share/BoxHeader/index.js
+++ src/components/Share/BoxHeader/index.js
@@ -4,9 +4,9 @@ import './style.scss';
 
 export default function BoxHeader({ title, description }) {
   return (
-    <div className="box-header__wrapper">
-      <div className='box-header__title'>{title}</div>
-      <div className='box-header__description'>{description}</div>
+    <div className="box-header-card__wrapper">
+      <div className='box-header-card__title'>{title}</div>
+      <div className='box-header-card__description'>{description}</div>
     </div>
   )
 }
