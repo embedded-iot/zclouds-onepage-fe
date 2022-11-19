66454d1a4e1063d30df571e7445c3f2b451b2f65
Nguyen Van Quan <nguyenquan5895@gmail.com>
2022-11-19 19:39:06 +0700

    admin sider

--- src/components/Admin/Sider/index.js
+++ src/components/Admin/Sider/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Sider, { getItem } from 'components/Common/Sider';
 import { ROUTERS } from 'components/contants';
+import Icon from 'components/Common/Icon';
+import myAccountIcon from 'images/my-account-icon.png';
+import productCategoryIcon from 'images/product-category-icon.png';
+import categoriesIcon from 'images/orders-icon-1.png';
 
 export default function AdminSider({ selectedRouters = [], redirectTo = () => {}, }) {
   const items = [
-    getItem('Products management', ROUTERS.ADMIN_PRODUCTS_MANAGEMENT),
-    getItem('Categories management', ROUTERS.ADMIN_CATEGORIES_MANAGEMENT),
-    getItem('Users management', ROUTERS.ADMIN_ROLES_MANAGEMENT)
+    getItem('Products management', ROUTERS.ADMIN_PRODUCTS_MANAGEMENT, <Icon src={categoriesIcon} />),
+    getItem('Categories management', ROUTERS.ADMIN_CATEGORIES_MANAGEMENT, <Icon src={productCategoryIcon} />),
+    getItem('Users management', ROUTERS.ADMIN_ROLES_MANAGEMENT, <Icon src={myAccountIcon} />),
     // getItem('Admins and Roles', ROUTERS.ADMIN_ADMINS_AND_ROLES_MANAGEMENT, undefined, [
     //   getItem('Admins management', ROUTERS.ADMIN_ADMINS_MANAGEMENT),
     //   getItem( 'Roles management', ROUTERS.ADMIN_ROLES_MANAGEMENT),
