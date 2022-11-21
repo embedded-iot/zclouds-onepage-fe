756dff42e73c2e8d54d1ca1795875c397ad831ff
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-21 17:42:23 +0700

    Update index.js

--- src/containers/Seller/OrderDetailPage/index.js
+++ src/containers/Seller/OrderDetailPage/index.js
@@ -4,28 +4,61 @@ import { push } from 'connected-react-router';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import PageHeader from 'components/Share/PageHeader';
-import EditOrderBox from 'components/Seller/EditOrderBox';
-import { Col, Row } from 'antd';
+import AddEditOrderBox from 'components/Seller/AddEditOrderBox';
+import { ROUTERS } from 'components/contants';
 
 function OrderDetailPage(props) {
-  const { orderId } = props.match ? props.match.params : {};
+  const orderId = parseInt(props.match.params.orderId);
+  const isEdit = !!orderId;
+  const pageTitle = isEdit ? 'Edit Order' : 'Create Order';
+  const pageDescription = isEdit ? `Order ID: ${orderId}` : 'Great job, your dashboard is ready to go! Grow your business with Lenful.';
+
+  const order = {
+    "productId": 0,
+    "productName": "string",
+    "quantity": 0,
+    "mockupUrl": "http://localhost:3000/orders/0",
+    "designUrl": "http://localhost:3000/orders/0",
+    "storeId": 0,
+    "orderNumber": "string",
+    "orderNote": "string",
+    "orderShipping": {
+      "fullName": "string",
+      "phoneNumber": "string",
+      "address1": "string",
+      "address2": "string",
+      "country": "string",
+      "zipCode": "string",
+      "region": "string",
+      "city": "string"
+    }
+  };
+  const data = {
+    ...order,
+    ...order.orderShipping,
+  }
+
+  const goOrdersPage = () => {
+    props.push(ROUTERS.SELLER_ORDERS);
+  }
+
   return (
     <div className="page-wrapper">
       <Helmet>
-        <title>Edit Order</title>
+        <title>{pageTitle}</title>
       </Helmet>
       <PageHeader
-        title="Orders"
-        description={`Order ID: ${orderId}`}
+        title={pageTitle}
+        description={pageDescription}
       />
       <div className="page-contents">
-        <Row gutter={[20, 20]}>
-          <Col span={12}>
-            <EditOrderBox id={orderId}
-                          redirectTo={props.push}
-            />
-          </Col>
-        </Row>
+        <AddEditOrderBox
+          id={orderId}
+          data={isEdit ? data : undefined}
+          redirectTo={props.push}
+          onOk={goOrdersPage}
+          onCancel={goOrdersPage}
+        />
       </div>
     </div>
   );
