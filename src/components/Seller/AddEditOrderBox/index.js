0ba417fcba9b0b2eaccad3ec94a817ac4a824c94
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-21 17:40:02 +0700

    Update index.js

--- src/components/Seller/AddEditOrderBox/index.js
+++ src/components/Seller/AddEditOrderBox/index.js
@@ -1,16 +1,70 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Form, notification } from 'antd';
 import OrderForm from 'components/Seller/AddEditOrderBox/OrderForm';
-import { BaseService, SellerOrdersService } from 'services';
+import {
+  BaseService, FrontUserCategoriesService,
+  SellerDesignsService,
+  SellerOrdersService,
+  SellerStoresService,
+} from 'services';
+import BoxCard from 'components/Share/BoxCard';
+import { getStoresOptions } from 'services/Seller/StoresService';
+import { getDesignsOptions } from 'services/Seller/DesignsService';
+import { getProductsOptions } from 'services/FrontUser/CategoriesService';
 
-export default function AddEditOrderBox({ data, onOk, onCancel }) {
+import './style.scss';
+
+export default function AddEditOrderBox({ data, onOk, onCancel, redirectTo }) {
+  const [productsOptions, setProductsOptions] = useState([]);
+  const [storesOptions, setStoresOptions] = useState([]);
+  const [designsOptions, setDesignsOptions] = useState([]);
   const isEdit = !!data;
   const [form] = Form.useForm();
 
+  const getProducts = () => {
+    FrontUserCategoriesService.getCategories({ pageNum: 1, pageSize: 100 }, response => {
+      setProductsOptions(getProductsOptions(response.items));
+    }, () => {}, true)
+  }
+
+  const getStores = () => {
+    SellerStoresService.getStores({ pageNum: 1, pageSize: 100 }, response => {
+      setStoresOptions(getStoresOptions(response.items));
+    }, () => {}, true)
+  }
+
+  const getDesigns = () => {
+    SellerDesignsService.getDesigns({ pageNum: 1, pageSize: 100 }, response => {
+      setDesignsOptions(getDesignsOptions(response.items));
+    }, () => {}, true)
+  }
+
+  useEffect(() => {
+    getProducts();
+    getStores();
+    getDesigns();
+  }, []);
+
   const handleOk = (values) => {
-    const { slug, type } = values;
     const newData = {
-      slug, type
+      productId: values.productId,
+      productName: values.productName,
+      quantity: values.quantity,
+      mockupUrl: values.mockupUrl,
+      designUrl: values.designUrl,
+      storeId: values.storeId,
+      orderNumber: values.orderNumber,
+      orderNote: values.orderNote,
+      orderShipping: {
+        fullName: values.fullName,
+        phoneNumber: values.phoneNumber,
+        address1: values.address1,
+        address2: values.address2,
+        country: values.country,
+        zipCode: values.zipCode,
+        region: values.region,
+        city: values.city,
+      }
     }
     if (isEdit) {
       SellerOrdersService.updateOrder(newData, response => {
@@ -39,9 +93,17 @@ export default function AddEditOrderBox({ data, onOk, onCancel }) {
 
 
   return (
-    <OrderForm
-      form={form}
-      initialValues={data}
-    />
+    <BoxCard className={'add-edit-order-box__wrapper'}>
+      <OrderForm
+        form={form}
+        initialValues={data}
+        productsOptions={productsOptions}
+        storesOptions={storesOptions}
+        designsOptions={designsOptions}
+        redirectTo={redirectTo}
+        onFinish={handleOk}
+        onCancel={onCancel}
+      />
+    </BoxCard>
   )
 }
