bf06eb0e9099b9cd386cdc187f021377008c2abd
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-24 17:51:17 +0700

    Update index.js

--- src/components/Share/PageHeader/index.js
+++ src/components/Share/PageHeader/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
-
+import BreadcrumbBox from 'components/Common/BreadcrumbBox';
+import { ROUTERS } from 'components/contants';
 import './style.scss';
 
-export default function PageHeader({ title, description }) {
+export default function PageHeader({ title, description, currentBreadcrumb = '', breadcrumbRouters = []}) {
+  const showBreadcrumb = !!currentBreadcrumb || breadcrumbRouters.length;
+  const routers = [
+    ...(!!currentBreadcrumb ? [
+      {
+        path: ROUTERS.ROOT,
+        breadcrumbName: 'Dashboard',
+      },
+      {
+        breadcrumbName: currentBreadcrumb,
+      },
+    ] : []),
+    ...breadcrumbRouters,
+  ];
   return (
     <div className="box-header__wrapper">
       <div className='box-header__title'>{title}</div>
       <div className='box-header__description'>{description}</div>
+      {
+        !!showBreadcrumb && (
+          <BreadcrumbBox routes={routers} className="box-header__breadcrumb"/>
+        )
+      }
     </div>
   )
 }
