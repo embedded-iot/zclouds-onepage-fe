08c0bb2e164e049b728c085afbd4143b0fbf9b83
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-11-25 18:03:15 +0700

    Update index.js

--- src/components/Seller/EditStoreBox/index.js
+++ src/components/Seller/EditStoreBox/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+// eslint-disable-next-line
 import { notification } from 'antd';
 import ShopifyForm from './Vendors/ShopifyForm';
 import { BaseService, SellerIntegrationsService, SellerStoresService } from 'services';
