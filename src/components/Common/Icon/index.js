eec60714cfbba2d80431e8e0a2f630d55bba3e60
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-12-01 17:50:44 +0700

    Update index.js

--- src/components/Common/Icon/index.js
+++ src/components/Common/Icon/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export default function Icon({ src, alt, width, height, style }) {
+export default function Icon({ src, alt, width, height, style, ...rest }) {
   return (
-    <img src={src} alt={alt || 'icon'} width={width} height={height} style={style} />
+    <img src={src} alt={alt || 'icon'} width={width} height={height} style={style} {...rest} />
   );
 }
