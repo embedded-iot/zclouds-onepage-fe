49a82797238b667a8e0276a7b8096917014030e4
nguyenquan5895 <nguyenquan5895@gmail.com>
2022-12-01 17:51:35 +0700

    Update index.js

--- src/components/Common/InputText/index.js
+++ src/components/Common/InputText/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Input } from 'antd';
-
 import './style.scss';
 
-export default function InputText({ name, value, onChange, placeholder, theme,  ...restProps }) {
-  const handlerOnchange = (e) => onChange(e.target.value, name);
+const { TextArea } = Input;
 
+export default function InputText({ name, type, value, onChange, placeholder, theme,  ...restProps }) {
+  const handlerOnchange = (e) => onChange(e.target.value, name);
+  const InputComponent = type === 'TextArea' ? TextArea : Input;
   return (
     <div className={`input-text ${theme}`}>
-      <Input placeholder={placeholder}
+      <InputComponent placeholder={placeholder}
              value={value}
              onChange={handlerOnchange}
              {...restProps}
