c60e2415a215ebfd30bd89568b9bf07bc487b922
Nguyen Van Quan <nguyenquan5895@gmail.com>
2022-12-15 21:28:27 +0700

    Update format currency for negative number

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,9 +1,10 @@
 
-function formatCurrency(num = 0, unit = "$", isFloat = false) {
+function formatCurrency(number = 0, unit = "$", isFloat = false) {
+  const num = Math.abs(number);
   var p = num.toFixed(2).split(".");
-  return `${unit}` + p[0].split("").reverse().reduce(function(acc, num, i, orig) {
+  return `${number < 0 ? '-' : ''}` + p[0].split("").reverse().reduce(function(acc, num, i, orig) {
     return num + (num !== "-" && i && !(i % 3) ? "," : "") + acc;
-  }, "") + (isFloat  ? `.${p[1]}` : '');
+  }, "") + (isFloat  ? `.${p[1]}` : '') + ` ${unit}`;
 }
 
 export {
