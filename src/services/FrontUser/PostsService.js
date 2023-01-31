1a94f9aa9d8b3b0642bda512e21e002a8023ae64
nguyenquan5895 <nguyenquan5895@gmail.com>
2023-01-31 21:35:43 +0700

    Update image for home

--- src/services/FrontUser/PostsService.js
+++ src/services/FrontUser/PostsService.js
@@ -1,22 +1,24 @@
-import avatar from 'images/avatar.png';
+import avatar1 from 'images/avatar_1.jpg';
+import avatar2 from 'images/avatar_2.jpg';
+import avatar3 from 'images/avatar_3.jpg';
 import post from 'images/post.png';
 
 const items = [
   {
     content: '“I have to tell you that you are probably one of the best print-on-demand companies out there and they appreciate how you keep prices reasonable and shipping fees low, which is very important. Their customer service staffs are great. Just wanted to let you know that your zippered fleece hoodie is the warmest, softest, most comfortable hoodie I\'ve ever worn in my life.”',
-    avatar: avatar,
+    avatar: avatar1,
     name: 'Nguyen Minh Phuong',
     work: 'Seller'
   },
   {
-    content: 'CS- fulfillment is my favorite for online shipping. The sewing is improved after some initial models had some loose threads at the seams. Shipping times have improved and I mostly receive my items about two weeks after I place my order. My orders are only delivered to the United States. Products and services from CS-fulfillment are of good quality',
-    avatar: avatar,
+    content: 'CS- fulfillment is my favorite for online shippnig. The sewing is improved after some initial models had some loose threads at the seams. Shipping times have improved and I mostly receive my items about two weeks after I place my order. My orders are only delivered to the United States. Products and services from CS-fulfillment are of good quality',
+    avatar: avatar2,
     name: 'Pham Thi Thu',
     work: 'Seller'
   },
   {
     content: '“I have to say CS- fulfillment is one of the best on-demand companies out there and I appreciate how you keep your prices competitive and shipping fees low. The support team is impressive. They care about all that I need “',
-    avatar: avatar,
+    avatar: avatar3,
     name: 'Tran Thanh My',
     work: 'Seller'
   },
