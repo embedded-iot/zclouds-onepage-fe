6e62538f7253497d9b83f15988f9efe7208401f0
nguyenquan5895 <nguyenquan5895@gmail.com>
2023-03-02 08:13:33 +0700

    Update domains

--- src/components/Seller/DomainsTable/index.js
+++ src/components/Seller/DomainsTable/index.js
@@ -1,20 +1,29 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TableGrid from 'components/Common/TableGrid';
 import { SellerDomainsService } from 'services';
 import { authentication, cui,events } from 'utils';
 import { Button} from 'antd';
-import { PlusCircleOutlined, EditOutlined, CloseCircleOutlined } from '@ant-design/icons';
-import AddEditDomainModal from './AddEditDomainModal';
+import { EditOutlined, MinusCircleOutlined } from '@ant-design/icons';
+import AddEditDomainBox from './AddEditDomainBox';
 import DeleteDomainModal from './DeleteDomainModal';
 import BoxCard from 'components/Share/BoxCard';
 import StatusTag from 'components/Share/StatusTag';
 import { PERMISSION_VALUES, RESPONSIVE_MEDIAS } from 'components/contants';
 import { useMediaQuery } from 'react-responsive';
+import ButtonListWrapper from 'components/Common/ButtonListWrapper';
+
+const ACTION_KEYS = {
+  ACTION_EVENTS: "ACTION_EVENTS",
+  ADD_DOMAIN: "ADD_DOMAIN",
+  EDIT_DOMAIN: "EDIT_DOMAIN",
+  DELETE_DOMAIN: "DELETE_DOMAIN",
+}
 
 const columns = [
   {
-    title: 'Domain',
+    title: 'Domain Name',
     dataIndex: 'name',
+    render: (domain) => <span style={{ color: '#0066FF'}}>{domain}</span>
   },
   {
     title: 'Status',
@@ -23,23 +32,49 @@ const columns = [
       return (<StatusTag value={record.state} label={convertedStatus}/>);
     }
   },
-];
+  {
+    title: 'Action',
+    dataIndex: 'id',
+    render: (id, record) => {
+      const handleClick = (key) => {
+        events.publish(ACTION_KEYS.ACTION_EVENTS, {
+          key,
+          record,
+        })
+      }
+      const buttonList = [
+        authentication.getPermission(PERMISSION_VALUES.SELLER_ADD_EDIT_DOMAIN) && (
+          <Button
+            key={ACTION_KEYS.EDIT_DOMAIN}
+            icon={<EditOutlined />}
+            onClick={() => handleClick(ACTION_KEYS.EDIT_DOMAIN)}
+          >
+            Edit
+          </Button>
+        ),
+        authentication.getPermission(PERMISSION_VALUES.SELLER_DELETE_DOMAIN) && (
+          <Button
+            danger
 
-const ACTION_KEYS = {
-  ADD_DOMAIN: "ADD_DOMAIN",
-  EDIT_DOMAIN: "EDIT_DOMAIN",
-  DELETE_DOMAIN: "DELETE_DOMAIN",
-  EXPORT_DOMAINS: "EXPORT_DOMAINS",
-}
+            icon={<MinusCircleOutlined />}
+            onClick={() => handleClick(ACTION_KEYS.DELETE_DOMAIN)}
+          >
+            Remove
+          </Button>
+        )
+      ]
+      return (
+        <ButtonListWrapper buttonList={buttonList}/>
+      );
+    }
+  },
+];
 
 export default function DomainsTable() {
   const isMobile = useMediaQuery(RESPONSIVE_MEDIAS.MOBILE);
-  const [openAddDomain, setOpenAddDomain] = useState(false);
-  const [isEdit, setIsEdit] = useState(false);
   const [openDeleteDomain, setOpenDeleteDomain] = useState(false);
   const [selectedDomain, setSelectedDomain] = useState(null);
   // eslint-disable-next-line
-  const [selectedKeys, setSelectedKeys] = useState([]);
   const RELOAD_EVENT_KEY = 'RELOAD_ADMIN_DOMAINS_TABLE_EVENT_KEY';
   let ref = useRef({});
   const tableConfig = {
@@ -58,28 +93,11 @@ export default function DomainsTable() {
 
   const reloadTable = (filters ={}) => {
     setOpenDeleteDomain(false);
-    setOpenAddDomain(false);
     events.publish(RELOAD_EVENT_KEY, filters);
   }
 
-  const addDomain = () => {
-    setIsEdit(false);
-    setOpenAddDomain(true);
-  }
-
-  const editDomain = () => {
-    setIsEdit(true);
-    setOpenAddDomain(true);
-  }
-
-  const deleteDomain = () => {
-    setOpenDeleteDomain(true);
-  }
-
   const onSelectedItemsChange = (keys) => {
-    setSelectedKeys(keys);
-    const newSelectedDomain = ref.current.items.find(item => item.id === keys[0]);
-    setSelectedDomain(newSelectedDomain);
+
   }
 
   const headerActionsConfig = {
@@ -87,18 +105,6 @@ export default function DomainsTable() {
     gutter: [10, 10],
     className: isMobile && 'box-card--mobile',
     buttonList: [
-      {
-        type: 'custom',
-        render: <Button key={ACTION_KEYS.EDIT_DOMAIN} icon={<EditOutlined />} onClick={editDomain}>Edit Domain</Button>,
-        requiredSelection: true,
-        permission: authentication.getPermission(PERMISSION_VALUES.SELLER_ADD_EDIT_DOMAIN),
-      },
-      {
-        type: 'custom',
-        render: <Button key={ACTION_KEYS.DELETE_DOMAIN} icon={<CloseCircleOutlined />} type="primary" danger ghost onClick={deleteDomain}>Delete Domain</Button>,
-        requiredSelection: true,
-        permission: authentication.getPermission(PERMISSION_VALUES.SELLER_DELETE_DOMAIN),
-      },
       {
         type: 'searchText',
         span: 24,
@@ -127,50 +133,77 @@ export default function DomainsTable() {
           style: { width: '100%' }
         },
       },
-      {
-        type: 'custom',
-        render: <Button key={ACTION_KEYS.ADD_DOMAIN} type="primary" icon={<PlusCircleOutlined />} style={{ width: isMobile ? '100%' : 'auto' }}  onClick={addDomain}>Add Domain</Button>,
-        span: 12,
-        align: 'right',
-        permission: authentication.getPermission(PERMISSION_VALUES.SELLER_ADD_EDIT_DOMAIN),
-      }
     ],
   }
+
+  const editDomain = domain => {
+
+  }
+
+  const deleteDomain = domain => {
+    setSelectedDomain(domain);
+    setOpenDeleteDomain(true)
+  }
+
+  const actionListenerFunc = () => {
+    let reloadListener = null;
+    reloadListener = events.subscribe(ACTION_KEYS.ACTION_EVENTS, ({ key, record }) => {
+      switch (key) {
+        case ACTION_KEYS.EDIT_DOMAIN:
+          editDomain(record);
+          break;
+        case ACTION_KEYS.DELETE_DOMAIN:
+          deleteDomain(record);
+          break;
+        default:
+      }
+    });
+    return reloadListener;
+  }
+
+  useEffect(() => {
+    const reloadListener = actionListenerFunc();
+    return () => {
+      reloadListener && reloadListener.remove();
+    };
+    // eslint-disable-next-line
+  }, []);
+
   const BoxWrapper = isMobile ? 'div' : BoxCard;
   return (
-    <BoxWrapper className={!isMobile && 'content-box__wrapper'}>
-      <TableGrid configs={tableConfig}
-                 headerActionsConfig={headerActionsConfig}
-                 paginationConfig={{}}
-                 defaultParams={{}}
-                 defaultData={{}}
-                 isShowPagination={true}
-                 isSingleSelection={true}
-                 onSelectedItemsChange={onSelectedItemsChange}
-                 isAllowSelection={authentication.getPermission(PERMISSION_VALUES.SELLER_ADD_EDIT_DOMAIN) ||
-                   authentication.getPermission(PERMISSION_VALUES.SELLER_DELETE_DOMAIN)}
-                 RELOAD_EVENT_KEY={RELOAD_EVENT_KEY}
-      />
-      {
-        openAddDomain && (
-          <AddEditDomainModal
-            open={openAddDomain}
-            data={isEdit ? selectedDomain : null}
-            onOk={reloadTable}
-            onCancel={() => { setOpenAddDomain(false); }}
-          />
-        )
-      }
-      {
-        openDeleteDomain && (
-          <DeleteDomainModal
-            open={openDeleteDomain}
-            data={selectedDomain}
-            onOk={reloadTable}
-            onCancel={() => { setOpenDeleteDomain(false); }}
-          />
-        )
-      }
-    </BoxWrapper>
+    <>
+      <BoxWrapper className={!isMobile && 'card-box__wrapper'}>
+        <TableGrid configs={tableConfig}
+                   headerActionsConfig={headerActionsConfig}
+                   paginationConfig={{}}
+                   defaultParams={{}}
+                   defaultData={{}}
+                   isShowPagination={true}
+                   isSingleSelection={true}
+                   onSelectedItemsChange={onSelectedItemsChange}
+                   isAllowSelection={false}
+                   RELOAD_EVENT_KEY={RELOAD_EVENT_KEY}
+        />
+        {
+          openDeleteDomain && (
+            <DeleteDomainModal
+              open={openDeleteDomain}
+              data={selectedDomain}
+              onOk={reloadTable}
+              onCancel={() => { setOpenDeleteDomain(false); }}
+            />
+          )
+        }
+      </BoxWrapper>
+      <BoxWrapper className={!isMobile && 'card-box__wrapper'}>
+        {
+          authentication.getPermission(PERMISSION_VALUES.SELLER_ADD_EDIT_DOMAIN) && (
+            <AddEditDomainBox
+              onOk={reloadTable}
+            />
+          )
+        }
+      </BoxWrapper>
+    </>
   );
 }
